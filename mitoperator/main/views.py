c455e8152e080f79caba5ecbcfd3ab5ebb1d237e
Brandon Martin-Anderson <brandon@operator.media.mit.edu>
2011-10-08 13:31:29 -0400

    start using templates

--- mitoperator/main/views.py
+++ mitoperator/main/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 from models import StopTimeUpdate
 from django.db.models import Count, Avg
@@ -15,21 +16,14 @@ from gtfs import Schedule
 sched = Schedule( 'massachusetts-archiver_20110913_0233.db' )
 
 def home(req):
-    n = StopTimeUpdate.objects.all().count()
-    n_trips = StopTimeUpdate.objects.values('trip_id').annotate(Count('trip_id')).count()
+    #n = StopTimeUpdate.objects.all().count()
+    #n_trips = StopTimeUpdate.objects.values('trip_id').annotate(Count('trip_id')).count()
 
-    delay = StopTimeUpdate.objects.filter( fetch_timestamp__gt = int(time())-60*5 ).aggregate(Avg("arrival_delay"))
-    recent_count = StopTimeUpdate.objects.filter( fetch_timestamp__gt = int(time())-60*5 ).values('trip_id').annotate(Count('trip_id')).count()
+    #delay = StopTimeUpdate.objects.filter( fetch_timestamp__gt = int(time())-60*5 ).aggregate(Avg("arrival_delay"))
+    #recent_count = StopTimeUpdate.objects.filter( fetch_timestamp__gt = int(time())-60*5 ).values('trip_id').annotate(Count('trip_id')).count()
 
-    return HttpResponse( """<html><body><p>%d data points</br>
-%s trips</p>
-<p>
-tracking %d vehicles</br>
-the whole system is %.2f mins late</br>
-</p>
-<p><a href="http://en.wikipedia.org/wiki/Operator_(The_Matrix)">operator</a>. get me out of here.</p>
-<p><a href="/mysite.fcgi/stops">stops</a></p>
-</body></html>"""%(n,n_trips, recent_count, delay['arrival_delay__avg']/60.0) )
+    #"""%(n,n_trips, recent_count, delay['arrival_delay__avg']/60.0) )
+    return render_to_response( "home.html", {} )
 
 def gtfs_timestr( time ):
     return "%02d:%02d:%02d"%((time/3600)%12,(time%3600)/60,time%60)
--- mitoperator/settings.py
+++ mitoperator/settings.py
@@ -103,6 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'mitoperator.urls'
 
 TEMPLATE_DIRS = (
+    "/home/brandon/operator/mitoperator/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
