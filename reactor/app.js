83db848d4acec89b857418e486647aa0208006e1
Brandon Martin-Anderson <brandon@operator.media.mit.edu>
2012-01-01 17:49:12 -0500

    query map

--- reactor/app.js
+++ reactor/app.js
@@ -1,23 +1,42 @@
-var app = require('http').createServer(handler)
+var app = require('http').createServer()
   , io = require('socket.io').listen(app)
+
+var pg = require('pg');
+
+io.settings["log level"] = 1;
+console.log( io );
+
+pg.defaults.database = 'operator';
+pg.defaults.password = 'password';
+
+var client = new pg.Client();
+client.connect();
 
 var width,height;
 
 app.listen(1337);
 
-function handler(req,res){
-}
-
-
 io.sockets.on('connection', function (socket) {
+  
 
-  socket.on('size', function(data) {
-    width=data[0];
-    height=data[1];
-  });
-  socket.on('line', function(data) {
-    //io.sockets.emit( 'line', [width-data[0], height-data[1], width-data[2], height-data[3]] );
-    io.sockets.emit( 'line', data );
+  socket.on('map', function(data) {
+    console.log( data );
+    var ll=data[0],bb=data[1],rr=data[2],tt=data[3];
+
+    var last_shape_id=null;
+    var query = client.query( "SELECT * FROM shapes WHERE (shape_pt_lat BETWEEN $1 AND $2) AND (shape_pt_lon BETWEEN $3 AND $4) ORDER BY shape_id, shape_pt_sequence", [bb,tt,ll,rr] );
+    query.on( 'row', function(row){
+      if(row.shape_id!=last_shape_id) {
+        socket.emit( 'newshape', row.shape_id );
+        console.log( "new shape id "+row.shape_id );
+      } else {
+        socket.emit( 'pt', [Math.round(parseFloat(row.shape_pt_lon)*10000), 
+                            Math.round(parseFloat(row.shape_pt_lat)*10000)] );
+      }
+
+      last_shape_id = row.shape_id;
+    });
   });
 
 });
+
