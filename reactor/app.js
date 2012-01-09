1a2d8d7399b3c0e47e8675872159ba87b6b6a7a5
Brandon Martin-Anderson <brandon@operator.media.mit.edu>
2012-01-09 14:07:24 -0500

    restrict query to bounding box

--- reactor/app.js
+++ reactor/app.js
@@ -24,7 +24,10 @@ io.sockets.on('connection', function (socket) {
     var ll=data[0],bb=data[1],rr=data[2],tt=data[3];
 
     var last_shape_id=null;
-    var query = client.query( "SELECT * FROM shapes WHERE (shape_pt_lat BETWEEN $1 AND $2) AND (shape_pt_lon BETWEEN $3 AND $4) ORDER BY shape_id, shape_pt_sequence", [bb,tt,ll,rr] );
+
+    qs = "SELECT * FROM shapes WHERE (shape_pt_lat BETWEEN $1 AND $2) AND (shape_pt_lon BETWEEN $3 AND $4) ORDER BY shape_id, shape_pt_sequence";
+    qs = "SELECT * FROM shapes";
+    var query = client.query( qs );//, [bb,tt,ll,rr] );
     query.on( 'row', function(row){
       if(row.shape_id!=last_shape_id) {
         socket.emit( 'newshape', row.shape_id );
